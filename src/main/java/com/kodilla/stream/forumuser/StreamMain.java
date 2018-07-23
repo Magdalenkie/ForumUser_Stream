a1951eb752e80a18fb8bf38314e7f957289a0ddb
Magdalenkie <magda.dabrowska1026@gmail.com>
2018-07-23 22:17:46 +0200

    funkcyjny spacer po liście użytkowników forum 2

--- src/main/java/com/kodilla/stream/forumuser/StreamMain.java
+++ src/main/java/com/kodilla/stream/forumuser/StreamMain.java
@@ -1,8 +1,5 @@
 package com.kodilla.stream.forumuser;
 
-import com.kodilla.stream.forumuser.ForumUser;
-import com.kodilla.stream.forumuser.Forum;
-
 import java.time.LocalDate;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,27 +10,27 @@ public class StreamMain {
         Forum theForum = new Forum();
 
         // odfiltruje tylko tych użytkowników, którzy są mężczyznami
-        Map<String, ForumUser> theResultMapOfForum = theForum.getForumUserList().stream()
+        Map<Integer, ForumUser> theResultMapOfForum = theForum.getForumUserList().stream()
                 .filter(forum -> forum.getSex() == 'M')
-                .collect(Collectors.toMap(ForumUser::getName, forum -> forum));
+                .collect(Collectors.toMap(ForumUser::getIDNum, forum -> forum));
 
         System.out.println("# elements: " + theResultMapOfForum.size());
         theResultMapOfForum.entrySet().stream().map(entry -> entry.getKey() + ": " + entry.getValue()).forEach(System.out::println);
 
 
        // odfiltruje tylko tych użytkowników, którzy mają co najmniej 20 lat
-        Map<String, ForumUser> theResultMapOfForum1 = theForum.getForumUserList().stream()
+        Map<Integer, ForumUser> theResultMapOfForum1 = theForum.getForumUserList().stream()
                 .filter(forumUser -> forumUser.getDateOfBirth().isBefore(LocalDate.now().minusYears(20)))
-                .collect(Collectors.toMap(ForumUser::getName, forum -> forum));
+                .collect(Collectors.toMap(ForumUser::getIDNum, forum -> forum));
 
         System.out.println("# elements: " + theResultMapOfForum1.size());
         theResultMapOfForum1.entrySet().stream().map(entry -> entry.getKey() + ": " + entry.getValue()).forEach(System.out::println);
 
 
        // odfiltruje tylko tych użytkowników, którzy mają co najmniej jeden opublikowany post
-        Map<String, ForumUser> theResultMapOfForum2 = theForum.getForumUserList().stream()
+        Map<Integer, ForumUser> theResultMapOfForum2 = theForum.getForumUserList().stream()
                 .filter(forumUser -> forumUser.getPostsQuantity() >= 1)
-                .collect(Collectors.toMap(ForumUser::getName, forum -> forum));
+                .collect(Collectors.toMap(ForumUser::getIDNum, forum -> forum));
 
         System.out.println("# elements: " + theResultMapOfForum2.size());
         theResultMapOfForum2.entrySet().stream().map(entry -> entry.getKey() + ": " + entry.getValue()).forEach(System.out::println);
